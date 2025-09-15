610d55d10fdcc29d50f08b68112eeb8ebf97f58f
Shafayat Ahammed <shafayatahammedofficial@gmail.com>
2025-09-15 18:35:39 +0600

    style(services): update service card background color to match site theme

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -63,12 +63,12 @@ const Services: FunctionComponent = (): JSX.Element => {
             return (
               <div
                 key={index}
-                className="bg-primary-blue p-4 rounded-lg cursor-default hover:scale-103 transition-transform flex flex-col gap-2"
+                className="bg-[radial-gradient(circle_farthest-corner_at_50%_0%,#1e2230_8%,#0e0e0f_54%,#090909_103%)] border border-gray-800 p-4 rounded-lg cursor-default hover:scale-103 transition-transform flex flex-col gap-2"
               >
                 <h3 className="text-white font-semibold text-base sm:text-lg md:text-xl 2xl:text-2xl">
                   {name}
                 </h3>
-                <p className="text-green-300 text-sm sm:text-base 2xl:text-[18.5px]">
+                <p className="text-primary-gray text-sm sm:text-base 2xl:text-[18.5px]">
                   {description}
                 </p>
               </div>
